b43751962f6b5e99818dc2992a5cbe4791efc560
augustus4400 <augustus4400@3f379a74-dcba-1b49-2840-92fbe12b0ddf>
2010-06-05 02:23:38 +0000

    first time checkin

---
+++ Samples/Nehe/nehegl_mfc/Main.h
@@ -0,0 +1,31 @@
+// Main.h: interface for the CMain class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_MAIN_H__05A74DD2_C37A_4085_A563_7ED88F625CB7__INCLUDED_)
+#define AFX_MAIN_H__05A74DD2_C37A_4085_A563_7ED88F625CB7__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CMain  
+{
+public:
+	
+	virtual BOOL Initialize();
+	virtual void Deinitialize();
+	virtual void Update (DWORD milliseconds);
+	virtual void Draw();
+
+public:
+	BOOL KeyPressed( int nCode );
+		
+	
+
+	CMain();
+	virtual ~CMain();
+
+};
+
+#endif // !defined(AFX_MAIN_H__05A74DD2_C37A_4085_A563_7ED88F625CB7__INCLUDED_)
